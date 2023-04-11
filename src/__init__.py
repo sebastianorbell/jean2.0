df792d42514f462cf8e3d6438fd422b24a7e7189
b-vanstraat <barnaby.vanstraaten@materials.ox.ac.uk>
2023-04-11 15:41:34 +0200

    fixed imports

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,4 @@ Created on 22/03/2023
 @author sebastian
 """
 from .Jean import Jean
-from .utils import Database
-from .helper_functions import Dummy_parameter
+from .utils import Database, Dummy_parameter
