88db392ae7d5554a6acce1f1bf49c478a3a2ead1
sebastiano <sebastian.o@btinternet.com>
2023-04-11 09:58:22 +0200

    updated dummy parametrs

---
+++ jean2/inference/main.py
@@ -0,0 +1,132 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+def decaying_cosine(t, frequency, amplitude, phase, offset, decay):
+    return amplitude * np.exp(-decay * t) * np.cos(2 * np.pi * frequency * t + phase) + offset
+
+
+def decaying_envelope_upper(t, frequency, amplitude, phase, offset, decay):
+    return amplitude * np.exp(-decay * t) + offset
+
+
+def decaying_envelope_lower(t, frequency, amplitude, phase, offset, decay):
+    return -amplitude * np.exp(-decay * t) + offset
+
+
+def dtft(Z, time_span, frequencies, subtract_mean = True):
+    periods = time_span * frequencies
+
+    if subtract_mean:
+        Z = Z - Z.mean()
+
+    n_0 = np.arange(0, Z.shape[0]) / Z.shape[0]
+    z_0 = np.exp(-2j * np.pi * periods[:, np.newaxis] * n_0[np.newaxis, :])
+
+    data = np.einsum('a, ba -> b', Z, z_0, optimize='greedy')
+    return np.abs(data) / Z.size
+
+
+def fit_decaying_sine(t, y, plot=True):
+    """
+    Fit a decaying sine to the data
+    :param t: the time points in ns
+    :param y: the measured demodulate signal
+    :param plot: a bool whether to plot the fit
+    :return: f_pred, T2_pred, signal_to_noise the predicted frequency and T2 in MHz and us respectively
+    """
+
+    y = (y - y.min()) / (y.max() - y.min())
+
+    # the time span for the measurement
+    time_span = t.max() - t.min()
+    delta_t = time_span / len(t)
+    f_niquist = 1 / (2 * delta_t)
+
+    print(f"Nyquist frequency: {f_niquist /  1e6:.2f}MHz")
+
+    # the frequencies to computer the fourier transform for
+    frequencies = np.arange(0, f_niquist, 1e4)
+
+    # computing the fourier transform
+    ft = dtft(y, time_span, frequencies)
+
+    # finding the maximum of the fourier transform
+    arg = np.argmax(ft)
+    f_est = frequencies[arg]
+    amplitude_est = (y.max() - y.min()) / 2.
+    phase_est = np.pi
+    offset_est = y.mean()
+    T2_est = 1e6
+
+    # the bounds for the fit
+    f_bounds = (0, f_niquist)
+    amplitude_bounds = (0, 2 * amplitude_est)
+    phase_bounds = (0, 2 * np.pi)
+    offset_bounds = (y.min(), y.max())
+    T2_bounds = (0, 10e6)
+    bounds = list(zip(f_bounds, amplitude_bounds, phase_bounds, offset_bounds, T2_bounds))
+
+    # the initial guess
+    p0 = [f_est, amplitude_est, phase_est, offset_est, T2_est]
+    popt, pcov = curve_fit(decaying_cosine, t, y, p0, bounds=bounds)
+
+    # extracting the important parameters from the fit
+    f_pred, amplitude_pred, phase_pred, offset_pred, gamma_T2_pred = popt
+    T2_pred = 1 / gamma_T2_pred
+
+    # changing units
+    T2_us = T2_pred * 1e6
+    f_MHz = f_pred / 1e6
+
+    # computing the predicted signal
+    y_pred = decaying_cosine(t, *popt)
+    ft_pred = dtft(y_pred, time_span, frequencies)
+
+    # computing the signal-to-noise ratio
+    least_squares_noise = np.sqrt(np.mean((y - y_pred) ** 2))
+    signal_to_noise = least_squares_noise / amplitude_pred
+    print(f"signal to noise: {signal_to_noise}")
+
+    # a block to plot the fit and the fourier transform
+    if plot:
+        t_plot = t * 1e9
+        frequencies_plot = frequencies / 1e6
+
+        fig, axs = plt.subplots(2)
+        fig.suptitle('Fit of function')
+        axs[0].plot(t_plot, y, label='data', marker='o', color='b')
+        axs[0].plot(t_plot, y_pred, label='fit', color='r')
+        axs[0].plot(t_plot, decaying_envelope_upper(t, *popt), color='k', alpha=0.5, linestyle='--')
+        axs[0].plot(t_plot, decaying_envelope_lower(t, *popt), color='k', alpha=0.5, linestyle='--')
+        axs[0].axhline(offset_pred, color='k', alpha=0.5, linestyle='--')
+
+        axs[0].legend()
+
+        axs[0].set_xlabel('Time (ns)')
+        axs[0].set_ylabel('Amplitude (V)')
+
+        axs[1].plot(frequencies_plot, ft, label='data')
+        axs[1].plot(frequencies_plot, ft_pred, label='fit', color='r')
+
+        axs[1].set_xlabel('Frequency (MHz)')
+        axs[1].set_ylabel('Fourier Amplitude (V)')
+        axs[1].axvline(f_est / 1e6, color='r', label='prior frequency')
+        axs[1].axvline(f_pred / 1e6, color='g', label='posterior frequency')
+        axs[1].legend()
+
+        plt.tight_layout()
+        plt.show()
+
+    return f_MHz, T2_us, signal_to_noise
+
+# t = np.arange(0, 5000, 50) / 1e9
+# y = decaying_cosine(t, 1e6, 1, np.pi, 1, 0.2e6) + np.random.randn(t.size) * 0.3
+# f, T2, signal_to_noise = fit_decaying_sine(t, y, plot=True)
+
+data = np.loadtxt( './data/Time.dat' )
+t = data[:,0] / 1e9
+y = data[:,1] * 1e5
+
+f, T2, signal_to_noise = fit_decaying_sine(t, y, plot=True)
